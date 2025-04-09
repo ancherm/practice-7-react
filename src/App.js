848757858f6411cdc24fc93c7fb29358c9e9fc6c
Ancherm <andrewcherm2014@gmail.com>
2025-04-09 09:22:59 +0400

    edit p

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Я из ci cd и ручной, добавил линтер222
+          Я из ci cd и ручной, добавил линтер333
         </p>
         <a
           className="App-link"
