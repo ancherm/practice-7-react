f6ef6b97e243c372ea5d3af41024a81f738b1804
Ancherm <andrewcherm2014@gmail.com>
2025-04-09 08:47:17 +0400

    edit p

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello aaa
+          Hell
         </p>
         <a
           className="App-link"
